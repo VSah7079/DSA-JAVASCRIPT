bd0669942eeec2d3fa77e0f9763aa4b10de05b45
VSah7079 <vk3815155@gmail.com>
2025-03-10 10:26:24 +0530

    Refactor Loop.js to remove commented-out code and update logic for strong number calculation

--- Loop.js
+++ Loop.js
@@ -128,7 +128,53 @@ for (var i = 1; i < 23; i++) {
 //   ans = prompt("kuch bhi dedo(exit for close)");
 // }
 
-let pr = prompt("Add number:");
+// let pr = prompt("Add number:");
+// if (pr === null) {
+//   console.log("Canclled");
+// } else {
+//   let n = Number(pr);
+
+//   if (isNaN(n)) {
+//     console.log("Invalid input");
+//   } else {
+//     if (n > 0) {
+//       let sum = 0;
+//       while (n > 0) {
+//         let rem = n % 10;
+//         sum = sum + rem;
+//         n = Math.floor(n / 10);
+//       }
+//       console.log(sum);
+//     } else {
+//       console.log("Should be +ve and more than 0");
+//     }
+//   }
+// }
+
+// reverse number using while loop
+// let pr = prompt("Reverse number:");
+// if (pr === null) {
+//   console.log("Canclled");
+// } else {
+//   let n = Number(pr);
+
+//   if (isNaN(n)) {
+//     console.log("Invalid input");
+//   } else {
+//     if (n > 0) {
+//       let rev = 0;
+//       while (n > 0) {
+//         let rem = n % 10;
+//         rev = rev * 10 + rem;
+//         n = Math.floor(n / 10);
+//       }
+//       console.log(rev);
+//     } else {
+//       console.log("Should be +ve and more than 0");
+//     }
+//   }
+// }
+let pr = prompt("Strong  number:");
 if (pr === null) {
   console.log("Canclled");
 } else {
@@ -138,13 +184,13 @@ if (pr === null) {
     console.log("Invalid input");
   } else {
     if (n > 0) {
-      let sum = 0;
+      let rev = 0;
       while (n > 0) {
         let rem = n % 10;
-        sum = sum + rem;
+        rev = rev * 10 + rem;
         n = Math.floor(n / 10);
       }
-      console.log(sum);
+      console.log(rev);
     } else {
       console.log("Should be +ve and more than 0");
     }
