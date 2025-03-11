aea94da34f5f9fc790f20f6dc20b1d12950ed875
VSah7079 <vk3815155@gmail.com>
2025-03-11 09:17:43 +0530

    Refactor Loop.js to comment out old strong number logic and guessing game code

--- Loop.js
+++ Loop.js
@@ -174,25 +174,55 @@ for (var i = 1; i < 23; i++) {
 //     }
 //   }
 // }
-let pr = prompt("Strong  number:");
-if (pr === null) {
-  console.log("Canclled");
-} else {
-  let n = Number(pr);
+// let pr = prompt("Strong  number:");
+// if (pr === null) {
+//   console.log("Canclled");
+// } else {
+//   let n = Number(pr);
 
-  if (isNaN(n)) {
-    console.log("Invalid input");
-  } else {
-    if (n > 0) {
-      let rev = 0;
-      while (n > 0) {
-        let rem = n % 10;
-        rev = rev * 10 + rem;
-        n = Math.floor(n / 10);
-      }
-      console.log(rev);
-    } else {
-      console.log("Should be +ve and more than 0");
-    }
-  }
-}
+//   if (isNaN(n)) {
+//     console.log("Invalid input");
+//   } else {
+//     if (n > 0) {
+//       let sum = 0;
+//       let copy = n;
+//       while (n > 0) {
+//         let rem = n % 10;
+//         let fact = 1;
+//         for (let i = 1; i <= rem; i++) {
+//           fact = fact * i;
+//         }
+//         sum = sum + fact;
+//         n = Math.floor(n / 10);
+//       }
+//       if (copy === sum) {
+//         console.log("Strong number");
+//       } else {
+//         console.log("Not a strong number");
+//       }
+//     } else {
+//       console.log("Should be +ve and more than 0");
+//     }
+//   }
+// }
+
+// let random = Math.floor(Math.random() * 100 + 1);
+
+// let guess = 0;
+// while (guess !== random) {
+//   guess = Number(prompt("Guess the number:"));
+
+//   if (isNaN(guess) || guess < 1 || guess > 100) {
+//     console.log("Try again between 1 to 100");
+//     continue;
+//   }
+//   if (guess > random) {
+//     console.log("Too high, try again ");
+//   } else if (guess < random) {
+//     console.log("Too low, try again ");
+//   } else {
+//     console.log("Congrats you guessed it right🤷‍♂️✅");
+//     break;
+//   }
+// }
+
