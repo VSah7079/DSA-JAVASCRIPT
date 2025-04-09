df282ce96e268cca11e807659ea45e816dabdc83
VSah7079 <vk3815155@gmail.com>
2025-04-09 23:19:16 +0530

    Refactor strings.js to comment out unused code examples and improve readability

--- strings.js
+++ strings.js
@@ -1,3 +1,4 @@
+/*
 let v = "vivek";
 console.log(v.length);
 console.log(v.slice(0, 3));
@@ -11,7 +12,9 @@ console.log(v.concat("_", " kumar"));
 console.log(v.trim());
 console.log(v.charAt(3));
 
+*/
 /*
+
 let s = prompt("Enter a string");
 let ispalindrome = true;
 let i = 0;
@@ -23,11 +26,12 @@ while (i < j) {
   }
   i++;
   j--;
-
 }
-if(ispalindrome)console.log("pallindrome");
+if (ispalindrome) console.log("pallindrome");
 else console.log("no pallindrome");
 */
+
+
 
 /*
 let s = prompt("Enter a string");
@@ -45,6 +49,7 @@ for (let i = 0; i < s.length - 1; i++) {
 console.log(toggle);
 */
 
+/*
 let s = prompt("Enter a number");
 
 let arr = new Array(128).fill(0);
@@ -59,6 +64,96 @@ for (let i = 0; i < arr.length; i++) {
     console.log(String.fromCharCode(i) + " appears at " + arr[i] + " times");
   }
 }
+  */
 
+/*
+let arr = [1, 2, 3, 4, 5];
+let copy = arr[arr.length - 1];
+for (let i = arr.length - 1; i > 0; i--) {
+  arr[i] = arr[i - 1];
+}
+arr[0] = copy;
+console.log(arr);
 
+for (let j = 1; j <= 4; j++) {
+  console.log(j + " execution");
 
+  for (let i = 1; i <= 3; i++) {
+    console.log("Hello");
+  }
+}
+*/
+/*
+
+*****
+*****
+*****
+*****
+*****
+*/
+
+// let n = 5;
+// for (i = 0; i < n; i++) {
+//   for (j = 0; j < n; j++) {
+//     document
+//   }
+//   console.log("");
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let space = "";
+//   for (let j = 1; j <= n; j++) {
+//     space += "*";
+//   }
+//   console.log(space);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let space = "";
+//   for (let j = 1; j <= i; j++) {
+//     space += "*";
+//   }
+//   console.log(space);
+// }
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let space = "";
+//   for (let j = 1; j <=n-i+1; j++) {
+//     space += "*";
+//   }
+//   console.log(space);
+// }
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let space = "";
+//   for (let j = 1; j <= i; j++) {
+//     space += j;
+//   }
+//   console.log(space);
+// }
+
+/*
+
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  let space = "";
+  for (let j = 1; j <= n - i + 1; j++) {
+    space += j;
+  }
+  console.log(space);
+}
+
+//leap year
+
+let year = prompt("Enter a year ");
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+  console.log("Leap year");
+} else {
+  console.log("Not a leap year");
+}
+
+*/
