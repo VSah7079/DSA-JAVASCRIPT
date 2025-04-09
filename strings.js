490cd194515865aa9b314925590f1c3f576c3291
VSah7079 <vk3815155@gmail.com>
2025-04-09 13:42:36 +0530

    Add strings.js with string manipulation examples and update index1.html to include the script

--- strings.js
+++ strings.js
@@ -11,3 +11,54 @@ console.log(v.concat("_", " kumar"));
 console.log(v.trim());
 console.log(v.charAt(3));
 
+/*
+let s = prompt("Enter a string");
+let ispalindrome = true;
+let i = 0;
+j = s.length - 1;
+while (i < j) {
+  if (s.charAt(i) != s.charAt(j)) {
+    ispalindrome = false;
+    break;
+  }
+  i++;
+  j--;
+
+}
+if(ispalindrome)console.log("pallindrome");
+else console.log("no pallindrome");
+*/
+
+/*
+let s = prompt("Enter a string");
+console.log(s);
+let toggle = "";
+for (let i = 0; i < s.length - 1; i++) {
+  let ch = s.charCodeAt(i);
+
+  if (ch >= 65 && ch <= 90) {
+    toggle = toggle + String.fromCharCode(ch + 32);
+  } else if (ch >= 97 && ch <= 122) {
+    toggle = toggle + String.fromCharCode(ch - 32);
+  }
+}
+console.log(toggle);
+*/
+
+let s = prompt("Enter a number");
+
+let arr = new Array(128).fill(0);
+
+for (let i = 0; i < s.length; i++) {
+  let indx = s.charCodeAt(i);
+  arr[indx] = arr[indx] + 1;
+}
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) {
+    console.log(String.fromCharCode(i) + " appears at " + arr[i] + " times");
+  }
+}
+
+
+
